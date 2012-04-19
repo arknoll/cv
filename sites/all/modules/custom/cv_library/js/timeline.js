ce183cdc6e303780d697b8d1080bf709a7670f62
Alex Knoll <arknoll@gmail.com>
2012-04-19 17:16:10 -0600

    first commit

---
+++ sites/all/modules/custom/cv_library/js/timeline.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+jQuery(document).ready(function(){
+  jQuery('div.view-timeline div.views-row').each(function(){
+    jQuery(this).
+  });
+});
